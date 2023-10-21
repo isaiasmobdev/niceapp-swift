48345d6c1d43761119a53f59d89540cf3c9ce497
Isaias Cardoso <isaias.sc1@gmail.com>
2023-10-21 17:02:30 -0300

    Initial Commit

---
+++ niceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  niceGame
+//
+//  Created by Isaias Cardoso on 21/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
